64adf65f171c7c29f88ab9472f07f77df481d329
Jonathan Silva <jonathan.silva167@gmail.com>
2024-02-05 13:30:54 -0500

    add counting_valeys

---
+++ index.js
@@ -0,0 +1,14 @@
+// Import required modules
+const countingValleys = require('./counting_valleys/counting_valleys.js');
+// Add more imports for your modules here
+
+// Add your code logic here
+
+// Example usage of the imported module
+// const result = countingValleys(/* pass your arguments here */);
+// console.log(result);
+
+// Add more code logic here
+
+// Run the application in the terminal
+console.log("Node.js application is running!");
